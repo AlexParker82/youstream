ed6adb643a6efdd524bad5f79213898550d535bb
AlexParker82 <noyes.parker@gmail.com>
2024-03-04 19:50:08 -0500

    updating styling and tetsing layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,29 @@
 import { UserButton } from "@clerk/nextjs";
+import CardComp from "@/comps/CardComp";
+
+
 
 export default function Home() {
   return (
-    <div className="h-screen">
-      <UserButton />
+
+    <div className="flex flex-col h-full">
+      <div className="flex justify-between items-center bg-slate-800">
+        <div className="p-2 m-2">
+          <h1 className="text-4xl">YouStream</h1>
+        </div>
+        <div className="p-2 m-2">
+          <UserButton afterSignOutUrl="/" />
+        </div>
+      </div>
+      <div className="flex h-full">
+        <div className="bg-slate-700 w-48 flex justify-center">
+          <p>Sidebar</p>
+        </div>
+        <div className="bg-white text-black w-full">
+          <CardComp />
+
+        </div>
+      </div>
     </div>
   );
 }
---
+++ comps/CardComp.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+const CardComp = () => {
+  return (
+    <Card className="w-fit m-3">
+      <CardHeader>
+        <CardTitle>Card Title</CardTitle>
+        <CardDescription>Card Description</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>Card Content</p>
+      </CardContent>
+      <CardFooter>
+        <p>Card Footer</p>
+      </CardFooter>
+    </Card>
+  )
+}
+export default CardComp;
